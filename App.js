ac7717cd64044b1aabc6671c912369b5507e551f
GONZALO ROJAS <rojasg476@gmail.com>
2021-03-24 10:26:42 -0600

    Feat: Create Flatlists
    Description: Create json file that contains our data aplication and display on a Flatlist
    
    Co-authored-by: Demis Alberto Rincón Martínez <darmfma@gmail.com>

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
-import data from './constants/data.json'
-import Card from './components/card.js'
-import List from './components/list.js'
+import { StyleSheet, View, SafeAreaView } from 'react-native';
+import data from './constants/data.json';
+import List from './components/list';
 
 export default function App() {
-
   const trendingList = data.filter(elem => {
     return elem.type === "Trending"
-  })
+  });
 
 
   const recentList = data.filter(elem => {
     return elem.type === "Recent"
-  })
+  });
 
 
 
---
+++ constants/index.js
@@ -0,0 +1,9 @@
+import { COLORS, SIZES, FONTS } from './theme';
+import Images from './images';
+
+const Theme = { COLORS, SIZES, FONTS }; 
+
+export {
+    Theme,
+    Images
+}
